dd00f280ab55afe7dc09723759bfa0e8a70aa552
Bruna <bruna.lima@veinteractive.com>
2015-12-16 16:55:35 +0000

    First commit

---
+++ Calculator/Controllers/MathController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Calculator.Controllers
+{
+    //HTTP GET methods for basic mathematical operations.
+    public class MathController : ApiController
+    {
+        [HttpGet]
+        public int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+
+        [HttpGet]
+        public int Subtract(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+
+        [HttpGet]
+        public int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+
+        [HttpGet]
+        public int Divide(int value1, int value2)
+        {
+            return value1 / value2;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "default";
+        }
+    }
+}
---
+++ Calculator/Web/js/calculator.js
@@ -0,0 +1,46 @@
+﻿
+
+$document.ready(function () {
+
+    $(":button").click(function () {
+        var value1 = $("#txtValue1").val();
+        var value2 = $("#txtValue2").val();
+        var arg = "value1=" + value1 + "&value2=" + value2;
+        var control = event.target || event.srcElement;
+        var id = control.id;
+
+        var urlString = "/api/math";
+
+        switch (id) {
+            case 'btnAdd':
+                urlString = urlString + "Add/?" + arg;
+                break;
+            case 'btnMinus':
+                urlString = urlString + "Subtract/?" + arg;
+                break;
+            case 'btnMultiply':
+                urlString = urlString + "Multiply/?" + arg;
+                break;
+            case 'btnDivide':
+                urlString = urlString + "Divide/?" + arg;
+                break;
+            default:
+                urlString = urlString + "Hello";
+        }
+
+        $.ajax({
+            url: urlString,
+            type: "GET",
+            dataType: 'json',
+            success: function (result) {
+                $("#txtResult").val(result);
+            },
+            error: function (xhr, status, error) {
+                var err = eval("(" + xhr.responseText + ")");
+                $("txtResult").val(err.Message)
+            }
+        })
+
+    });
+
+});
